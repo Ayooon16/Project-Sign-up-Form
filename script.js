7386ca1900cb36dd38c0cf27a55c150a15ab890e
ayooon <michaunapierala@gmail.com>
2024-03-15 19:43:30 +0100

    Add basic functionality

---
+++ script.js
@@ -0,0 +1,12 @@
+/* function check(){
+var input = document.getElementById('pass2');
+console.log('a')
+if(document.getElementById('pass1')!==input){
+    input.setCustomValidity('Password Must be Matching.');
+    input.reportValidity()
+}
+else{
+    input.setCustomValidity('');
+
+}
+} */
